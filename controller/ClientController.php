ee7330b0696b2f666dcdbb4a5037805fcbde784c
atefnajar <103906678+atefnajar@users.noreply.github.com>
2022-04-18 03:12:24 +0100

    Update ClientController.php

--- controller/ClientController.php
+++ controller/ClientController.php
@@ -2,12 +2,8 @@
 require_once '../dao/clientDao.php';
 require_once '../model/Client.class.php';
 switch ($_REQUEST['action']) {
-    case 'formadd':
-		header("location:../view/ajouterClient.php");
-        break;
-    case 'formupdate':
-        header("location:../view/updateClient.php?ref=".$_REQUEST['ref']);
-        break;
+
+        
     
         case 'add':
             $client=new Client();
@@ -18,17 +14,17 @@ switch ($_REQUEST['action']) {
             $client->setAdresse_fac($_REQUEST['adresse_fac']);
             $client->setPersonne_Contact($_REQUEST['personne_contact']);
 
-        clientDao::AddNewClient($client);
+        ClientDao::AddNewClient($client);
         header("location:../view/client.php");
         break;
 
         case 'update':
 			$client=new client();
-            $client->setidc($_REQUEST['idc']);
+            $client->setIdc($_REQUEST['idc']);
             $client->setMail($_REQUEST['mail']);
             $client->setNom($_REQUEST['nom']);
-            $client->setAdresse_exp($_REQUEST['date_exp']);
-            $client->setAdresse_fac($_REQUEST['date_fac']);
+            $client->setAdresse_exp($_REQUEST['adresse_exp']);
+            $client->setAdresse_fac($_REQUEST['adresse_fac']);
             $client->setPersonne_Contact($_REQUEST['personne_contact']);
 
             
