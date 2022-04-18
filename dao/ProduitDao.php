da33931d68b09b1efcebd86c65da4e3c4acee97d
atefnajar <103906678+atefnajar@users.noreply.github.com>
2022-04-18 03:13:53 +0100

    Update ProduitDao.php

--- dao/ProduitDao.php
+++ dao/ProduitDao.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../model/Connexion.class.php';
-require_once '../model/produit.class.php';
+require_once '../model/Produit.class.php';
 
 class ProduitDao{
 
@@ -9,27 +9,19 @@ class ProduitDao{
 		$connexionbd=new ConnexionBD();
 		return $connexionbd->query("select * from produit");
 		
-
-    }
-    public static function GetAllProduit1($champ,$valeur)
-	{
-		$connexionbd=new ConnexionBD();
-		return $connexionbd->query("select * from produit where $champ=$valeur");
-		
-
     }
  
     public static function AddNewProduit($produit)
     {//insert produit $produit objet
         $connexionbd=new ConnexionBD();
-        return $connexionbd->exec("insert into produit values('".$produit->getReference()."','".$produit->getCout_unitaire()."', '".$produit->getDescription()."','".$produit->getDateCreation()."' )");
+        return $connexionbd->exec("insert into produit values('". $produit->getReference()."','".$produit->getCout_unitaire()."', '".$produit->getDescription()."','".$produit->getDateCreation()."')");
     }    
 
     public static function UpdateProduit($produit)
     {//update produit 
 	$connexionbd=new ConnexionBD();
 
-	return $connexionbd->exec("update produit set reference='".$produit->getReference()."' ,cout_unitaire='".$produit->getCout_unitaire()."', dateCreation='".$produit->getDateCreation()."', description='".$produit->getDescription()."'");
+	return $connexionbd->exec("update produit set  cout_unitaire='".$produit->getCout_unitaire()."', description='".$produit->getDescription()."', dateCreation='".$produit->getDateCreation()."'");
     }
     
     public static function DeleteProduit($reference)
@@ -43,7 +35,6 @@ class ProduitDao{
     {//affichage de produit dans un objet condition sur $reference
 	$connexionbd=new ConnexionBD();
 	return $connexionbd->query("select * from produit where reference=$reference");
-   
     }
 }
 ?>    
