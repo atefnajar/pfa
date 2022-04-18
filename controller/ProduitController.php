860d234e6530ae8e551c485da139eae5dc9ad185
atefnajar <103906678+atefnajar@users.noreply.github.com>
2022-04-18 03:13:04 +0100

    Update ProduitController.php

--- controller/ProduitController.php
+++ controller/ProduitController.php
@@ -2,13 +2,7 @@
 require_once '../dao/ProduitDao.php';
 require_once '../model/Produit.class.php';
 switch ($_REQUEST['action']) {
-    case 'formadd':
-		header("location:../view/ajoutercond.php");
-        break;
-    case 'formupdate':
-        header("location:../view/updateProduit.php?ref=".$_REQUEST['ref']);
-        break;
-    
+   
         case 'add':
             $produit=new Produit();
             $produit->setReference($_REQUEST['reference']);
@@ -20,15 +14,14 @@ switch ($_REQUEST['action']) {
         break;
 
         case 'update':
-			$produit=new produit();
+			$produit=new Produit();
             $produit->setReference($_REQUEST['reference']);
             $produit->setDateCreation($_REQUEST['dateCreation']);
             $produit->setDescription($_REQUEST['description']);
             $produit->setCout_unitaire($_REQUEST['cout_unitaire']);
-            
         
-        produitDao::Updateproduit($produit);
-        header("location:../view/produit.php");
+            ProduitDao::UpdateProduit($produit);
+             header("location:../view/produit.php");
         break;    
 
         case 'delete':
